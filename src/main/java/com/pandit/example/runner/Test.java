7a59f5a629689b02c56ffa64e45aaea5eb72de2d
Pandit1795 <101470562+Pandit1795@users.noreply.github.com>
2022-04-10 23:02:42 +0530

    Update Test.java

--- src/main/java/com/pandit/example/runner/Test.java
+++ src/main/java/com/pandit/example/runner/Test.java
@@ -15,7 +15,7 @@ public class Test {
 	public void run(String... args) throws Exception{
 		System.out.println(pob);
 		System.out.println(config);
-		System.out.println("Hiii");
+		System.out.println("Hiii Nikita");
 	}
 
 }
